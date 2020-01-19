6166cac6628ee8061456ea6dbc62ac4154282c3a
Nat Zimmermann <nat@ntzm.io>
2020-01-19 09:52:53 +0000

    add docker-compose for testing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 putenv('S3_REGION=foo');
 putenv('S3_AUTH_KEY=foo');
 putenv('S3_AUTH_SECRET=foo');
-putenv('S3_AUTH_BUCKET=foo');
+putenv('S3_BUCKET=foo');
 putenv('S3_ENDPOINT=http://localhost:4572');
 
 Dotenv::createMutable(__DIR__ . '/../', '.env.test')->safeLoad();
